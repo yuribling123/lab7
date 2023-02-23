ee382ccece69037e52dd772eac3d111baccfd6ba
Xiaoqing Liu <cs15lwi23aha@ieng6-201.ucsd.edu>
2023-02-23 15:38:15 -0800

    changes made

--- ListExamples.java
+++ ListExamples.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
-
+//this file has been changed
 class ListExamples {
 
   // Returns a new list that has all the elements of the input list for which
